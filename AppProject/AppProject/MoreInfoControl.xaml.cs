34ececb37a87b9148a720e9c3a9ad3a523453681
nguc <c.ngu.91@gmail.com>
2018-03-20 23:00:33 -0600

    commiting my changes to more info control

--- AppProject/AppProject/MoreInfoControl.xaml.cs
+++ AppProject/AppProject/MoreInfoControl.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,17 +14,37 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace SkipTheServer
+namespace AppProject
 {
     /// <summary>
     /// Interaction logic for MoreInfoControl.xaml
     /// </summary>
     public partial class MoreInfoControl : UserControl
     {
-        public MoreInfoControl()
+        // This lets me add info to the object whenever it is initialized
+        public MoreInfoControl(Image image, string imageName)
         {
             InitializeComponent();
+            this.Food_Name.Text = imageName;
+            this.Description.Text = readDescription(imageName);
+            this.Food_Image.BeginInit();
+            this.Food_Image.Source = image.Source;
+            this.Food_Image.EndInit();
         }
+
+        private string readDescription(string itemName)
+        {
+            string text = "Didn't find a descripion";
+            try
+            {
+              text = File.ReadAllText(@"Descriptions\" + itemName + ".txt");
+                Console.WriteLine(text);  
+            }
+            catch (FileNotFoundException ex){ Console.WriteLine(ex); }
+            
+            return text;
+        }
+
 
         private void Close_Click(object sender, RoutedEventArgs e)
         {
