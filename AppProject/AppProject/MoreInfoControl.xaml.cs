1f2fe895165de7cd08706ea6c97be5437c131735
unknown <nicholas.imperius@cs.ucalgary.ca>
2018-03-25 14:42:19 -0600

    Fixed Price error

--- AppProject/AppProject/MoreInfoControl.xaml.cs
+++ AppProject/AppProject/MoreInfoControl.xaml.cs
@@ -65,7 +65,7 @@ namespace AppProject
 
         public FoodItem ReturnFoodItem()
         {
-            FoodItem item = new FoodItem(this.Food_Name.Text, this.Price.ToString());
+            FoodItem item = new FoodItem(this.Food_Name.Text, this.Price.Text);
             return item;
         }
 
